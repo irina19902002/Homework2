6a2627dfbe309a132871c393000750dee6478bfe
Irina Smirnova <smirnovaiv20.02@gmail.com>
2023-01-16 22:53:13 +0300

    1

--- Massiv_zikl_uslov.java
+++ Massiv_zikl_uslov.java
@@ -52,21 +52,19 @@ public class Massiv_zikl_uslov {
         int nummes;
         int mes = 0;
         int mesU = 0;
-        String[] meses = {"Январь","Февраль","Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+        String[] meses = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
         do {
             mes++;
             uvelichenie = telovklada * 2.0 / 100;
             telovklada = telovklada + uvelichenie;
-            if (uvelichenie > 30)
-            {
+            if (uvelichenie > 30) {
                 mesU = mes;
             }
 
         }
         while (telovklada <= 1200);
         System.out.println("Вклад превысит 1200 через " + mes + " месяцев");
-        if (uvelichenie <= 30)
-        {
+        if (uvelichenie <= 30) {
             do {
                 mes++;
                 uvelichenie = telovklada * 2.0 / 100;
@@ -76,18 +74,18 @@ public class Massiv_zikl_uslov {
             while (uvelichenie <= 30);
             mesU = mes;
         }
-            if (mesU > 10) {
-                nummes = (1 + mesU % 12);
-               System.out.println("величина ежемесячного увеличения вклада превысит 30 руб за " + meses[nummes] + " месяц");
-                System.out.println("Прошло с начала вклада " + mesU + " месяцa");
-            }
-           else
-            {System.out.println("величина ежемесячного увеличения вклада превысит 30 руб за " + meses[1+mes] + " месяц");
-            System.out.println("Прошло с начала вклада " + mesU + " месяцa");}
-
+        if (mesU > 10) {
+            nummes = (1 + mesU % 12);
+            System.out.println("величина ежемесячного увеличения вклада превысит 30 руб за " + meses[nummes] + " месяц");
+            System.out.println("Прошло с начала вклада " + mesU + " месяцa");
+        } else {
+            System.out.println("величина ежемесячного увеличения вклада превысит 30 руб за " + meses[1 + mes] + " месяц");
+            System.out.println("Прошло с начала вклада " + mesU + " месяцa");
         }
 
     }
+
+}
 
 
 
